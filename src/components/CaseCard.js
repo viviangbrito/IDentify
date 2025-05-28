3e3776705a7dc9e02ca820754b387c4e0a43bd07
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-05-28 15:02:25 -0300

    modified:   src/components/CaseCard.js

--- src/components/CaseCard.js
+++ src/components/CaseCard.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, IconButton } from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
 
-// export default function CaseCard() {
-//     return (
+export default function CaseCard() {
+    return (
+        <View style={styles.card}>
+            
+        </View>
+    )
+}
 
-//     )
-// }
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#e6ddd3',
+        borderRadius: 8,
+        padding: 15,
+        marginVertical: 8,
+        shadowColor: '#3C260D',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        width: '90%',
+        alignSelf: 'center',
+    },
+})
